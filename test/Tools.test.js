2a79e5327c462005d5d7e0075bd44ac893835704
Baptiste Mazin <baptiste.mazin@gmail.com>
2020-02-05 14:13:02 +0100

    Add test template.

---
+++ test/Tools.test.js
@@ -0,0 +1,11 @@
+/* global describe test expect */
+import Tools from "../src/Tools.js";
+
+
+describe("Testing Tools", () => {
+
+    test("Fake test", () => {
+        expect(true).toBe(true);
+    });
+
+});
